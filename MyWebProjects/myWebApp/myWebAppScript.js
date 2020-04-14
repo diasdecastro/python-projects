83d33f76c96d3e95f0f83f3f0d85aa1f7f4fc104
diasdecastro <cristian.diass@hotmail.com>
2020-04-14 02:06:49 +0200

    commit

--- MyWebProjects/myWebApp/myWebAppScript.js
+++ MyWebProjects/myWebApp/myWebAppScript.js
@@ -1,28 +1,3 @@
-function hello(){
-    var el = document.createElement("h1");
-    var txt = document.createTextNode("Hello Again");
-    el.appendChild(txt);
-    var tag = document.getElementById("id1")
-    tag.appendChild(el)
-};
-
-function createUnsortedArray(){
-    arr = new Array();
-    for(var i = 0; i < 200; i++){
-        arr[i] = i + 1;
-    }
-    arr.sort(() => Math.random() - 0.5);
-    return arr
-};
-
-function drawLines(){
-    arr = createUnsortedArray();
-    for(var i = 0; i < arr.length; i++){
-        drawLine(arr[i]*2.7);
-    }
-
-};
-
 function drawLine(length){  
     var my_length = parseInt(length) + "px";
     var line = document.createElement("span");
@@ -30,8 +5,52 @@ function drawLine(length){
     line.style.borderLeft = "3px solid black";
     line.style.height = my_length;
     line.style.position = "relative";
-    line.style.float = "left";
-    line.style.margin = "0.1px";
+    line.style.margin = "1px";
     var tag = document.getElementById("contentBox");    
     tag.appendChild(line);
-};
+}
+
+function drawLines(arr){
+    container = document.getElementById("contentBox");
+    container.innerHTML = '';
+    for(var i = 0; i < arr.length; i++){ 
+        drawLine(arr[i]*2.7);
+    }
+}
+
+function createUnsortedArray(){
+    arr = new Array();
+    for(var i = 0; i < 200; i++){
+        arr[i] = i + 1;
+    }
+    arr.sort(() => Math.random() - 0.5);
+    return arr;
+}
+
+function generateArray(){
+    container = document.getElementById("contentBox");
+    container.innerHTML = '';
+    arr = createUnsortedArray();
+    drawLines(arr);
+}
+
+function bubbleSort(arr){
+    container = document.getElementById("contentBox");    
+    Array.prototype.swap = function(x, y){
+        var b = this[x];
+        this[x] = this[y];
+        this[y] = b;
+        return this;
+    }   
+    for(var i = arr.length; i > 1; i--){
+        for(var j = 0; j < arr.length - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                arr.swap(j, j+1);                                                  
+            }                        
+        }
+        setTimeout(function(){drawLines(arr);}, 1);         
+    }    
+    /* container = document.getElementById("contentBox");
+    container.innerHTML = ''; 
+    return drawLines(arr) */
+}
